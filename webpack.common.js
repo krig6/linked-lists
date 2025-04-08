cd025257e043c0c457aab91f1833bb69f7e2f023
krig6 <ej.ldsm@gmail.com>
2025-04-08 11:13:24 +0800

    chore: Update project name, description, and title
    - Updated the name and description fields in package.json to reflect the
    current project
    - Changed the title in webpack.common.js to match the updated project
    name

--- webpack.common.js
+++ webpack.common.js
@@ -35,7 +35,7 @@ export const plugins = [
   new HTMLWebpackPlugin({
     filename: 'index.html',
     template: 'src/template.html',
-    title: '',
+    title: 'Project: Linked Lists',
     favicon: ''
   })
 ];
