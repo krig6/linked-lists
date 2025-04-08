43910c9b9451d7c5946539b928b229936cbcd493
krig6 <ej.ldsm@gmail.com>
2025-04-08 12:03:51 +0800

    feat: Add 'append' method to linked list
    - Implemented the 'append' method to add a new node to the end of the
    linked list
    - If the list is empty, the new node becomes the head
    - Size is incremented after appending the node

--- src/index.js
+++ src/index.js
@@ -10,4 +10,19 @@ class LinkedList {
     this.head = null;
     this.size = 0;
   }
+
+  // Adds a new node containing value to the end of the list
+  append(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.nextNode !== null) {
+        currentNode = currentNode.nextNode;
+      }
+      currentNode.nextNode = newNode;
+    }
+    this.size++;
+  }
 }
